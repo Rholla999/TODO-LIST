bef913a651ee813b028b6201e5e9db7960ee64b3
MEMBA, Aku <104360361+Rholla999@users.noreply.github.com>
2025-03-09 23:32:11 +0100

    TODOLIST

--- my-react-app/src/Todo.jsx
+++ my-react-app/src/Todo.jsx
@@ -1,10 +1,31 @@
+import { useState } from 'react';
 
 function Todo() {
 
+    const [list, setList] = useState(['Visit Orphanage', 'Buy Groceries', 'Pay Bills', 'Call Mom']);
 
-    return (    
+    const addBtn = () => {
+        let input = document.getElementById('addList').value;
+        document.getElementById('addList').value = ''
+        if (input === '') {
+           setList([...list]);
+           return;
+        }
+        setList([...list, input]);
+    }
+
+    return (  
         <>
-        <h2>TO DO List</h2>
+        <div className='container'>
+            <h2>To Do List</h2>
+            <ul>
+                {list.map((item, index) =>(
+                <li key={index}> {item} </li>
+                ))}
+            </ul>
+            <input type="text" placeholder='things to do' id='addList' />
+            <button onClick={addBtn}>ADD</button>
+        </div>
         </>
     )
 }
