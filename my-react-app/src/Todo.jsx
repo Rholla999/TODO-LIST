70ef798d9d8543172b0627a12937bc80c70d2360
MEMBA, Aku <104360361+Rholla999@users.noreply.github.com>
2025-04-15 00:11:28 +0100

    added localStorage

--- my-react-app/src/Todo.jsx
+++ my-react-app/src/Todo.jsx
@@ -1,22 +1,60 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { FaTrashAlt } from "react-icons/fa"
 
 function Todo() {
 
-    const [list, setList] = useState([]);
+    function  getStoredData() {
+        let data = localStorage.getItem('list');
+        let json = JSON.parse(data);
 
-    const addBtn = () => {
-        let input = document.getElementById('addList').value;
-        document.getElementById('addList').value = ''
-        if (input === '') {
-           setList([...list]);
-           return;
+        if (json) {
+            return json;
         }
-        setList([...list, input]);
+        return [];
     }
+
+    const [list, setList] = useState(getStoredData());
+    const inputRef = useRef(null)
+
+
+    useEffect(() => {
+        localStorage.setItem('list', JSON.stringify(list))
+    }, [list])
+
+    //     useEffect(() => {
+    //         let getList = JSON.parse(localStorage.getItem('list'))
+        
+    //         if (getList) {
+    //             setList(getList)
+    //         }
+    //     }, [])
+
+    // useEffect(() => {
+    //     localStorage.setItem('list', JSON.stringify(list))
+    // }, [list])
+ 
+    const addBtn = () => {
+        const input = inputRef.current.value;
+        inputRef.current.value = ''
+        if (input === '') 
+            return ;
+
+        const newItem = {
+            text: input,
+            completed: false
+        };
+        setList([...list, newItem]);
+    }
+
     function removeItem(index){
         setList(list.filter((_, i) => i !== index))
     }
+
+    const toggleCompleted = (index) => {
+        const updatedList = [...list];
+        updatedList[index].completed = !updatedList[index].completed;
+        setList(updatedList);
+    };
 
     function handleEnterKey(e){
         if (e.key === "Enter"){
@@ -30,14 +68,26 @@ function Todo() {
             <ul>
                 {list.map((item, index) =>(
                 <li key={index} className='list'> 
-                <input type="checkbox"  />
-                {item} 
+                <input 
+                type="checkbox"
+                checked={item.completed}
+                onChange={() => toggleCompleted(index)}  />
+                  <span
+                            style={{
+                                textDecoration: item.completed ? 'line-through' : 'none',
+                                marginLeft: '8px',
+                                marginRight: '8px'
+                            }}
+                        >
+                            {item.text}
+                        </span>
+                {/* {item}  */}
                 {/* <button onClick={() => removeItem(index)} className='delBtn'>x</button> */}
                 <FaTrashAlt role='button' tabIndex= '0' className='delBtn' onClick={() => removeItem(index)}/>
                 </li>
                 ))}
             </ul>
-            <input type="text" placeholder='task' id='addList'  onKeyDown={handleEnterKey}/>
+            <input type="text" placeholder='task' ref={inputRef}  onKeyDown={handleEnterKey}/>
             <button onClick={addBtn} className='addBtn'>ADD</button>
         </div>
         </>
